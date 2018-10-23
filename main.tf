63bad604a480aa46e66c0c3feeb7ebf6582cf3c4
Anmol Nagpal <ianmolnagpal@gmail.com>
2018-10-23 21:55:37 +0200

    name fix

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_user" "this" {
+resource "aws_iam_user" "user" {
   count = "${var.create_user}"
 
   name          = "${var.name}"
@@ -6,25 +6,25 @@ resource "aws_iam_user" "this" {
   force_destroy = "${var.force_destroy}"
 }
 
-resource "aws_iam_user_login_profile" "this" {
+resource "aws_iam_user_login_profile" "profile" {
   count = "${var.create_user && var.create_iam_user_login_profile ? 1 : 0}"
 
-  user                    = "${aws_iam_user.this.name}"
+  user                    = "${aws_iam_user.user.name}"
   pgp_key                 = false
   password_length         = "${var.password_length}"
   password_reset_required = "${var.password_reset_required}"
 }
 
-resource "aws_iam_access_key" "this" {
+resource "aws_iam_access_key" "key" {
   count = "${var.create_user && var.create_iam_access_key ? 1 : 0}"
 
-  user = "${aws_iam_user.this.name}"
+  user = "${aws_iam_user.user.name}"
 }
 
-resource "aws_iam_user_ssh_key" "this" {
+resource "aws_iam_user_ssh_key" "ssh" {
   count = "${var.create_user && var.upload_iam_user_ssh_key ? 1 : 0}"
 
-  username   = "${aws_iam_user.this.name}"
+  username   = "${aws_iam_user.user.name}"
   encoding   = "${var.ssh_key_encoding}"
   public_key = "${var.ssh_public_key}"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,54 +1,54 @@
 output "this_iam_user_name" {
   description = "The user's name"
-  value       = "${element(concat(aws_iam_user.this.*.name, list("")), 0)}"
+  value       = "${element(concat(aws_iam_user.user.*.name, list("")), 0)}"
 }
 
 output "this_iam_user_arn" {
   description = "The ARN assigned by AWS for this user"
-  value       = "${element(concat(aws_iam_user.this.*.arn, list("")), 0)}"
+  value       = "${element(concat(aws_iam_user.user.*.arn, list("")), 0)}"
 }
 
 output "this_iam_user_unique_id" {
   description = "The unique ID assigned by AWS"
-  value       = "${element(concat(aws_iam_user.this.*.unique_id, list("")), 0)}"
+  value       = "${element(concat(aws_iam_user.user.*.unique_id, list("")), 0)}"
 }
 
 output "this_iam_user_login_profile_encrypted_password" {
   description = "The encrypted password, base64 encoded"
-  value       = "${element(concat(aws_iam_user_login_profile.this.*.encrypted_password, list("")), 0)}"
+  value       = "${element(concat(aws_iam_user_login_profile.profile.*.encrypted_password, list("")), 0)}"
 }
 
 output "this_iam_access_key_id" {
   description = "The access key ID"
-  value       = "${element(concat(aws_iam_access_key.this.*.id, list("")), 0)}"
+  value       = "${element(concat(aws_iam_access_key.key.*.id, list("")), 0)}"
 }
 
 output "this_iam_access_key_key_fingerprint" {
   description = "The fingerprint of the PGP key used to encrypt the secret"
-  value       = "${element(concat(aws_iam_access_key.this.*.key_fingerprint, list("")), 0)}"
+  value       = "${element(concat(aws_iam_access_key.key.*.key_fingerprint, list("")), 0)}"
 }
 
 output "this_iam_access_key_encrypted_secret" {
   description = "The encrypted secret, base64 encoded"
-  value       = "${element(concat(aws_iam_access_key.this.*.encrypted_secret, list("")), 0)}"
+  value       = "${element(concat(aws_iam_access_key.key.*.encrypted_secret, list("")), 0)}"
 }
 
 output "this_iam_access_key_ses_smtp_password" {
   description = "The secret access key converted into an SES SMTP password"
-  value       = "${element(concat(aws_iam_access_key.this.*.ses_smtp_password, list("")), 0)}"
+  value       = "${element(concat(aws_iam_access_key.key.*.ses_smtp_password, list("")), 0)}"
 }
 
 output "this_iam_access_key_status" {
   description = "Active or Inactive. Keys are initially active, but can be made inactive by other means."
-  value       = "${element(concat(aws_iam_access_key.this.*.status, list("")), 0)}"
+  value       = "${element(concat(aws_iam_access_key.key.*.status, list("")), 0)}"
 }
 
 output "this_iam_user_ssh_key_ssh_public_key_id" {
   description = "The unique identifier for the SSH public key"
-  value       = "${element(concat(aws_iam_user_ssh_key.this.*.ssh_public_key_id, list("")), 0)}"
+  value       = "${element(concat(aws_iam_user_ssh_key.ssh.*.ssh_public_key_id,list("")), 0)}"
 }
 
 output "this_iam_user_ssh_key_fingerprint" {
   description = "The MD5 message digest of the SSH public key"
-  value       = "${element(concat(aws_iam_user_ssh_key.this.*.fingerprint, list("")), 0)}"
+  value       = "${element(concat(aws_iam_user_ssh_key.ssh.*.fingerprint, list("")), 0)}"
 }
